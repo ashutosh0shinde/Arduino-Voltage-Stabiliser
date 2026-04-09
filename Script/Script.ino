d3cfb710ee2f50e672e9ad7ea445abb13977a934
ashutosh0shinde <shindeashutosh110@gmail.com>
2026-04-09 22:35:28 +0530

    Add feature to measure Voltage
    added two functions SetupVoltage() and MeasureVoltage(), setupvoltage fills the values in the array at the start of program and then measurevoltage takes the moving average of voltage repeatedly.

--- Script/Script.ino
+++ Script/Script.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+const int voltageReadPin = A0;
 
+int ind = 0;
+float volArr[20];
+float sum = 0;
+
+float voltage = 0;
+
+
+void MeasureVoltage();
+void SetupVoltage();
+
+void setup() 
+{
+  Serial.begin(9600);
+  SetupVoltage();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  MeasureVoltage();
+  Serial.println(voltage);
+  delay(50);
+}
 
+
+void SetupVoltage()
+{
+  for(int i = 0;i < 20; i++)
+  {
+    volArr[i] = analogRead(voltageReadPin) * (5.0/1023.0);
+    sum += volArr[i];
+    delay(10);
+  }
+  voltage = sum/20.0;
+}
+void MeasureVoltage() // taking moving average to smooth out the voltage
+{
+    sum -= volArr[ind];
+    volArr[ind] = analogRead(voltageReadPin) * (5.0/1023.0);
+    sum += volArr[ind];
+    ind = (ind + 1)%20;
+
+    voltage = sum/20;
 }
