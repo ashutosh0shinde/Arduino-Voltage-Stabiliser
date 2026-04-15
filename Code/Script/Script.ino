cb0146dc6950b5f88e9ed9e5c4d4faa0b1a6c3d9
ashutosh0shinde <shindeashutosh110@gmail.com>
2026-04-15 15:34:23 +0530

    Implement two point calibration for accurate mains calculation

--- Code/Script/Script.ino
+++ Code/Script/Script.ino
@@ -1,33 +1,51 @@
 const int voltageReadPin = A0;
 
+//voltage measurement
 int ind = 0;
 #define ARRLEN 10
 float volArr[ARRLEN];
 float sum = 0;
+float voltage = 0;
+float mains = 0;
+
+const double v_change = 0.004312;
 
 //Caklibration of voltage   voltage * (actual * sensed)
 float calib_correction = 0.60/0.62;
 
-float voltage = 0;
+//Two point calibration 
+float x1 = 153.0, x2 = 237.0;
+float y1 = 150.0, y2 = 243.0;
+float k,c;
+
 
 
 void MeasureVoltage();
 void SetupVoltage();
+float MainsToSense(int ms);
 
 void setup() 
 {
   Serial.begin(9600);
   analogReference(INTERNAL);
-  SetupVoltage();
 
+//Two point calibration
+  k = (x2-x1)/(y2-y1); //slope
+  c = x1 - (y1 * k); //offset
+  
+  SetupVoltage();
 }
 
 void loop()
 {
   MeasureVoltage();
-  Serial.println(voltage);
+  Serial.print(voltage,3);
+  Serial.print(" ");
+  Serial.println(mains);
   delay(100);
 }
+
+float MainsToSense(int ms) { return v_change * ms;} 
 
 
 void SetupVoltage()
@@ -40,6 +58,7 @@ void SetupVoltage()
     delay(10);
   }
   voltage = (sum/ARRLEN) * calib_correction;
+  mains = mains * k + c;// 2 point calib
 }
 void MeasureVoltage() // taking moving average to smooth out the voltage
 {
@@ -49,4 +68,6 @@ void MeasureVoltage() // taking moving average to smooth out the voltage
     ind = (ind + 1)%ARRLEN;
 
     voltage = (sum/ARRLEN) * calib_correction;
+    mains = voltage / v_change;
+    mains = mains * k + c;// 2 point calib
 }
