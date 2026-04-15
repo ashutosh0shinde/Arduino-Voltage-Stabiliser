f3a89bd57951e20d3130540eab68fb5bae7da6db
ashutosh0shinde <shindeashutosh110@gmail.com>
2026-04-15 10:17:45 +0530

    add calibration for voltage measurement

--- Code/Script/Script.ino
+++ Code/Script/Script.ino
@@ -1,9 +1,12 @@
-const int voltageReadPin = A5;
+const int voltageReadPin = A0;
 
 int ind = 0;
 #define ARRLEN 10
 float volArr[ARRLEN];
 float sum = 0;
+
+//Caklibration of voltage   voltage * (actual * sensed)
+float calib_correction = 0.60/0.62;
 
 float voltage = 0;
 
@@ -14,6 +17,7 @@ void SetupVoltage();
 void setup() 
 {
   Serial.begin(9600);
+  analogReference(INTERNAL);
   SetupVoltage();
 
 }
@@ -31,18 +35,18 @@ void SetupVoltage()
   sum = 0;
   for(int i = 0;i < ARRLEN; i++)
   {
-    volArr[i] = analogRead(voltageReadPin) * (4.94/1023.0);
+    volArr[i] = analogRead(voltageReadPin) * (1.1/1023.0);
     sum += volArr[i];
     delay(10);
   }
-  voltage = sum/ARRLEN;
+  voltage = (sum/ARRLEN) * calib_correction;
 }
 void MeasureVoltage() // taking moving average to smooth out the voltage
 {
     sum -= volArr[ind];
-    volArr[ind] = analogRead(voltageReadPin) * (4.94/1023.0);
+    volArr[ind] = analogRead(voltageReadPin) * (1.1/1023.0);
     sum += volArr[ind];
     ind = (ind + 1)%ARRLEN;
 
-    voltage = sum/ARRLEN;
+    voltage = (sum/ARRLEN) * calib_correction;
 }
